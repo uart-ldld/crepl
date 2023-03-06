181781a2181118a0e938d1e6df54996b51607bce
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2023-03-06 13:15:01 +0100

    Fixed run-traces.sh

--- script/run-traces.sh
+++ script/run-traces.sh
@@ -2,11 +2,12 @@
 
 result_dir="result-$(date +'%Y-%m-%d_%H-%M')-$(git rev-parse --short HEAD)"
 bin_dir="ChampSim/bin-$(git rev-parse --short HEAD)"
+trace_dir="trace"
 
 mkdir -p $result_dir
 
-for trace in trace/*.champsimtrace.xz; do
-    for bin in $bin_dir; do
+for trace in $trace_dir/*.champsimtrace.xz; do
+    for bin in $bin_dir/*; do
         sbatch -A naiss2023-22-203 \
                -p core \
                -n 1 \
