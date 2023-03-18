22ad2a14e5f274c0105f35ab335d0c1d5eb10e3c
Xiaoyue Chen <xiaoyue.chen.0484@student.uu.se>
2023-03-18 01:40:52 +0100

    Updated crrip

--- script/generate-crrip-configs.sh
+++ script/generate-crrip-configs.sh
@@ -4,14 +4,23 @@
 
 make_config_dir
 
-generate_config "crrip" \
-                ".load_branch_criticality = true | \
-                 .load_load_criticality = true | \
+generate_config "crrip-load-load" \
+                ".load_load_criticality = true | \
+                 .load_branch_criticality = false | \
                  .L1D.replacement = \"crrip\" | \
                  .L2C.replacement = \"crrip\" | \
                  .LLC.replacement = \"crrip\""
 
-generate_config "srrip" \
-                ".L1D.replacement = \"srrip\" | \
-                 .L2C.replacement = \"srrip\" | \
-                 .LLC.replacement = \"srrip\""
+generate_config "crrip-load-branch" \
+                ".load_load_criticality = false | \
+                 .load_branch_criticality = true | \
+                 .L1D.replacement = \"crrip\" | \
+                 .L2C.replacement = \"crrip\" | \
+                 .LLC.replacement = \"crrip\""
+
+generate_config "crrip-both" \
+                ".load_load_criticality = true | \
+                 .load_branch_criticality = true | \
+                 .L1D.replacement = \"crrip\" | \
+                 .L2C.replacement = \"crrip\" | \
+                 .LLC.replacement = \"crrip\""
